387d42b56d51eb6a8d0132375248d67ed104d3fa
bisaksss <bisaksss@gmail.com>
2021-07-07 14:34:29 +0530

    complete portfolio using dummy data

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -1,53 +1,65 @@
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
+import {featuredData,webData,mobileData} from "../../data";
+import { useEffect } from "react";
 
 export default function Portfolio() {
+    const [selected,setSelected]=useState("featured");
+    const [data,setData]=useState([]);
+    const list =[
+        {
+            id:"featured",
+            title:"Featured"
+        },
+        {
+            id:"web",
+            title:"Web"
+        },
+        {
+            id:"mobile",
+            title:"Mobile"
+        }
+    ];
+
+    useEffect(()=>{
+        switch(selected){
+            case "featured":
+                setData(featuredData);
+                break;
+            case "web":
+                setData(webData);
+                break;
+            case "mobile":
+                setData(mobileData);
+                break;
+            default:   
+                setData(featuredData); 
+        }
+    },[selected]);
     return (
         <div className="portfolio" id="portfolio"> 
 
         <h1>Portfolio</h1>
         <ul>
-            <li className="active">Featured</li>
-            <li>Web App</li>
-            <li>Mobile App</li>
-            <li>Branding</li>
+        {list.map((item)=>(
+            <PortfolioList title={item.title} active={selected===item.id} setSelected={setSelected} id={item.id}/>
+        ))}
         </ul>
         <div className="conatiner">
-            <div className="items">
 
-                <img src="assets/portfolio.png" alt="" />
-                <h3>Branding App</h3>
-
-            </div>
-            <div className="items">
+            {data.map((itemData)=>(
                 
-                <img src="assets/portfolio.png" alt="" />
-                <h3>Branding App</h3>
+                    <div className="items">
 
-            </div>
-            <div className="items">
-                
-                <img src="assets/portfolio.png" alt="" />
-                <h3>Branding App</h3>
+                      <img src={itemData.img} alt="" />
+                      <h3>{itemData.title}</h3>
+      
+                    </div>
 
-            </div>
-            <div className="items">
-                
-                <img src="assets/portfolio.png" alt="" />
-                <h3>Branding App</h3>
+            ))}
+      
 
-            </div>
-            <div className="items">
-                
-                <img src="assets/portfolio.png" alt="" />
-                <h3>Branding App</h3>
-
-            </div>
-            <div className="items">
-                
-                <img src="assets/portfolio.png" alt="" />
-                <h3>Branding App</h3>
-
-            </div>
           
         </div>
         
---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const featuredData=[
+    {
+        id:1,
+        title:"Title F1",
+        img:"assets/portfolio.png"
+
+    },
+    {
+        id:2,
+        title:"Title F2",
+        img:"assets/portfolio.png"
+
+    },
+    {
+        id:3,
+        title:"Title F3",
+        img:"assets/portfolio.png"
+
+    }
+];
+
+export const webData=[
+    {
+        id:1,
+        title:"Title W1",
+        img:"assets/portfolio.png"
+
+    },
+    {
+        id:2,
+        title:"Title W2",
+        img:"assets/portfolio.png"
+
+    }
+];
+
+
+export const mobileData=[
+    {
+        id:1,
+        title:"Title M1",
+        img:"assets/portfolio.png"
+
+    },
+    {
+        id:2,
+        title:"Title M2",
+        img:"assets/portfolio.png"
+
+    }
+];
